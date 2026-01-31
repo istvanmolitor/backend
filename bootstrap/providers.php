19af1342b5a1f4c1fa3bc2d014b9e2bd554d2bc5
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-01-31 02:05:49 +0100

    languages

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Molitor\Language\Providers\LanguageServiceProvider::class,
+    Molitor\User\Providers\UserServiceProvider::class,
+    Molitor\Menu\Providers\MenuServiceProvider::class,
+    Molitor\Admin\Providers\AdminServiceProvider::class,
 ];
