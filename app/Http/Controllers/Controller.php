e9ced9f301585d8876f75b99d24a95a9c550ccb9
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-02-02 11:01:01 +0100

    swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(title: "My API", version: "0.1")]
+#[OA\SecurityScheme(
+    securityScheme: "sanctum",
+    type: "apiKey",
+    name: "Authorization",
+    in: "header",
+    description: "Enter token in format (Bearer <token>)"
+)]
 abstract class Controller
 {
     //
