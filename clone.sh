dab4b88abfd4f0c224939f34a46b9af644d65239
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-02-26 11:34:50 +0100

    clone.sh

---
+++ clone.sh
@@ -0,0 +1,7 @@
+mkdir packages
+cd packages
+git clone git@github.com:istvanmolitor/admin.git
+git clone git@github.com:istvanmolitor/user.git
+git clone git@github.com:istvanmolitor/cms.git
+git clone git@github.com:istvanmolitor/language.git
+git clone git@github.com:istvanmolitor/media.git
